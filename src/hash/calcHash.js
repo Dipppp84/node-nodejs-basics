857037185e217075cea6b222f897d5e92d1bced8
dippp <Dipppp84@gmail.com>
2022-06-02 01:09:08 +0300

    feat: implementation hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,11 @@
+import crypto from 'crypto'
+import fs from 'fs'
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const myPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'fileToCalculateHashFor.txt');
+    const dataFile = await fs.promises.readFile(myPath);
+    const hash = crypto.createHash('sha256');
+    console.log(hash.update(dataFile).digest('hex'))
 };
