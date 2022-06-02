efb35cffa60a78190eed39b5136698af8d030658
dippp <Dipppp84@gmail.com>
2022-06-02 15:27:58 +0300

    feat: implementation zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import zlib from 'zlib'
+import fs from 'fs'
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const compress = async () => {
-    // Write your code here 
+    const sourcePath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'fileToCompress.txt');
+    const destinationPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'archive.gz');
+    const source = fs.createReadStream(sourcePath);
+    const destination = fs.createWriteStream(destinationPath);
+    const gzip = zlib.createGzip();
+    source.pipe(gzip)
+        .pipe(destination);
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,14 @@
+import zlib from 'zlib'
+import fs from 'fs'
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const decompress = async () => {
-    // Write your code here 
+    const sourcePath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'archive.gz');
+    const destinationPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'fileToCompress.txt');
+    const source = fs.createReadStream(sourcePath);
+    const destination = fs.createWriteStream(destinationPath);
+    const gzip = zlib.createUnzip();
+    source.pipe(gzip)
+        .pipe(destination);
 };
