797b69fcc46a19d189c1c62bbb8dc053b86fb7f8
dippp <Dipppp84@gmail.com>
2022-06-02 13:09:10 +0300

    feat: implementation streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,14 @@
+import {Transform} from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+    class MyTransform extends Transform {
+        _transform(chunk, encoding, callback) {
+            callback(null, chunk.reverse().toString().trimStart() + '\n');
+        }
+    }
+
+    const myTransform = new MyTransform();
+    process.stdin
+        .pipe(myTransform)
+        .pipe(process.stdout);
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,9 @@
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const write = async () => {
-    // Write your code here 
-};
+    const myPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'fileToWrite.txt');
+    const writeStream = fs.createWriteStream(myPath);
+    process.stdin.pipe(writeStream);
+}
