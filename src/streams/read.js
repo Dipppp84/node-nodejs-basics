e84f6369c1aa182e6668c8614dcb5831b9733974
dippp <Dipppp84@gmail.com>
2022-06-02 13:09:10 +0300

    feat: implementation streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,9 @@
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const read = async () => {
-    // Write your code here 
+    const myPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'fileToRead.txt');
+    const readStream = fs.createReadStream(myPath);
+    readStream.pipe(process.stdout);
 };
