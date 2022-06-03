b6fea0e125d39a65e75344d67dba519090f70d4a
dippp <Dipppp84@gmail.com>
2022-06-03 21:55:24 +0300

    feat: implementation wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import wt from "worker_threads";
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const performCalculations = async () => {
-    // Write your code here
+    const arrWorkers = [];
+    const myPath = path.join(fileURLToPath(import.meta.url), '..', 'worker.js');
+    for (let i = 0; i < process.env.NUMBER_OF_PROCESSORS; i++) {
+        arrWorkers.push(new wt.Worker(myPath));
+        arrWorkers[i].postMessage(10 + i);
+    }
+    const result = new Array(arrWorkers.length);
+    for (let i = 0; i < arrWorkers.length; i++) {
+        result[i] = new Promise(resolve => {
+            arrWorkers[i].on('message', obj => {
+                resolve(result[i] = obj);
+            });
+            arrWorkers[i].on('error', () => {
+                resolve(result[i] = {status: 'error', data: null});
+            });
+        });
+    }
+    console.log(await Promise.all(result));
 };
