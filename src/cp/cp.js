5489d9f7d9fe3a760c45ecfcf4dedfb438725bba
dippp <Dipppp84@gmail.com>
2022-06-03 22:47:01 +0300

    feat: implementation cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,8 @@
+import cp from "child_process"
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const srcPath = path.join(fileURLToPath(import.meta.url), '..', 'files', 'script.js');
+    const child = cp.fork(srcPath, args);
 };
